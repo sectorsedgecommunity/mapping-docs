93b05486f54385f05cd4ad46dea669a9da433fbe
RainlessSky <174295816+RainlessSky@users.noreply.github.com>
2024-07-02 14:53:08 -0700

    add search to site

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -31,6 +31,9 @@ export default defineConfig({
         ]
       }
     ],
+    search: {
+      provider: 'local'
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/sectorsedgecommunity/mapping-docs' }
