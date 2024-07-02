cabec222b60718c2ebf2b890d7a278e0f6b253b4
RainlessSky <174295816+RainlessSky@users.noreply.github.com>
2024-07-02 16:25:33 -0700

    Add favicon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   title: "Map Making",
   description: "Mapmaking documentation for Sector's Edge",
   base: "/mapping-docs/",
